9cb7bf6ad8b87616a4e65851895c144d451dd294
4989 <mbaascommit@gmail.com>
2015-05-20 16:40:09 +0900

    [ADD] added test about file delete

---
+++ test/files_test.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var config = require("config");
+var expect = require("chai").expect;
+var NCMB = require("../lib/ncmb");
+
+describe("NCMB Files", function(){
+  var ncmb = new NCMB();
+  ncmb
+  .set("apikey", config.apikey)
+  .set("clientkey", config.clientkey);
+
+  if(config.apiserver){
+    ncmb
+    .set("protocol", config.apiserver.protocol || "http:")
+    .set("fqdn", config.apiserver.fqdn)
+    .set("port", config.apiserver.port)
+    .set("proxy", config.apiserver.port || "");
+  }
+
+  describe("ユーザー削除", function(){
+    describe("delete", function(){
+      context("成功した場合", function(){
+        var del_file = new ncmb.File({fileName: "del_file.text"});
+
+        it("callback でレスポンスを取得できる", function(done){
+          del_file.delete(function(err){
+            done(err ? err : null);
+          });
+        });
+
+        it("promise でレスポンスを取得できる", function(done){
+          del_file.delete()
+          .then(function(){
+            done();
+          })
+          .catch(function(err){
+            done(err);
+          });
+        });
+      });
+    });
+
+    context("失敗した理由が", function(){
+      context("ObjectId がないときに", function(){
+        var del_file = new ncmb.File({});
+
+        it("callback で削除結果を取得できる", function(done){
+          del_file.delete(function(err){
+            expect(err).to.be.an.instanceof(Error);
+            done();
+          });
+        });
+
+        it("promise で削除結果を取得できる", function(done){
+          del_file.delete()
+              .then(function(){
+                done(new Error("失敗すべき"));
+              })
+              .catch(function(err){
+                expect(err).to.be.an.instanceof(Error);
+                done();
+              });
+        });
+      });
+    });
+  });
+});
+

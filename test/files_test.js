e31a73035a862f5d46e5473e66704767c272de15
4989 <mbaascommit@gmail.com>
2015-05-22 09:14:40 +0900

    [FIX]fixed test case of delete file

--- test/files_test.js
+++ test/files_test.js
@@ -18,41 +18,39 @@ describe("NCMB Files", function(){
     .set("proxy", config.apiserver.port || "");
   }
 
-  describe("ユーザー削除", function(){
-    describe("delete", function(){
-      context("成功した場合", function(){
-        var del_file = new ncmb.File({fileName: "del_file.text"});
+  describe("ファイル削除", function(){
+    context("成功した場合", function(){
+      var del_file = new ncmb.File({fileName: "del_file.text"});
 
-        it("callback でレスポンスを取得できる", function(done){
-          del_file.delete(function(err){
-            done(err ? err : null);
-          });
+      it("callback でレスポンスを取得できる", function(done){
+        del_file.delete(function(err){
+          done(err ? err : null);
         });
+      });
 
-        it("promise でレスポンスを取得できる", function(done){
-          del_file.delete()
-          .then(function(){
-            done();
-          })
-          .catch(function(err){
-            done(err);
-          });
+      it("promise でレスポンスを取得できる", function(done){
+        del_file.delete()
+        .then(function(){
+          done();
+        })
+        .catch(function(err){
+          done(err);
         });
       });
     });
 
     context("失敗した理由が", function(){
-      context("ObjectId がないときに", function(){
+      context("fileName がないときに", function(){
         var del_file = new ncmb.File({});
 
-        it("callback で削除結果を取得できる", function(done){
+        it("callback で削除時エラーを取得できる", function(done){
           del_file.delete(function(err){
             expect(err).to.be.an.instanceof(Error);
             done();
           });
         });
 
-        it("promise で削除結果を取得できる", function(done){
+        it("promise で削除時エラーを取得できる", function(done){
           del_file.delete()
               .then(function(){
                 done(new Error("失敗すべき"));

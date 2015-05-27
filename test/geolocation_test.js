5ba5ee26db7e3242a888d7b52c8803c05065b800
sci01445 <sci01445@nifty-5080.local>
2015-05-27 16:01:30 +0900

    [FIX] change parameter declaration scope to 'it'

--- test/geolocation_test.js
+++ test/geolocation_test.js
@@ -19,37 +19,37 @@ describe("NCMB Geolocation", function(){
   }
 
   describe("Geolocation正常設定", function(){
-    var geoObject1 = new ncmb.Geolocation();   // defaults to (0,0)
-    var geoObject2 = new ncmb.Geolocation(12,133);
-    var geoObject3 = new ncmb.Geolocation(30.0, 30.0);
-    var geoObject4 = new ncmb.Geolocation([30.0, 30.0]);
-    var geoObject5 = new ncmb.Geolocation({latitude: 30.0, longitude: 30.0});
     
     it("default check", function(done){
+      var geoObject1 = new ncmb.Geolocation();   // defaults to (0,0)
       expect(geoObject1.latitude).to.be.eql(0);
       expect(geoObject1.longitude).to.be.eql(0);
       done();
     });
 
     it("set integer check", function(done){
+      var geoObject2 = new ncmb.Geolocation(12,133);
       expect(geoObject2.latitude).to.be.eql(12);
       expect(geoObject2.longitude).to.be.eql(133);
       done();
     });
 
     it("set float check", function(done){
+      var geoObject3 = new ncmb.Geolocation(30.0, 30.0);
       expect(geoObject3.latitude).to.be.eql(30.0);
       expect(geoObject3.longitude).to.be.eql(30.0);
       done();
     });
 
     it("set array check", function(done){
+      var geoObject4 = new ncmb.Geolocation([30.0, 30.0]);
       expect(geoObject4.latitude).to.be.eql(30.0);
       expect(geoObject4.longitude).to.be.eql(30.0);
       done();
     });
 
     it("set json check", function(done){
+      var geoObject5 = new ncmb.Geolocation({latitude: 30.0, longitude: 30.0});
       expect(geoObject5.latitude).to.be.eql(30.0);
       expect(geoObject5.longitude).to.be.eql(30.0);
       done();

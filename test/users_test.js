c6342094a800b8600252a2869b9997047baf3087
4989 <mbaascommit@gmail.com>
2015-06-04 15:35:53 +0900

    [ADD]added test case for requestPasswordReset

--- test/users_test.js
+++ test/users_test.js
@@ -18,6 +18,58 @@ describe("NCMB Users", function(){
     .set("proxy", config.apiserver.port || "");
   }
 
+  describe("パスワード再発行メール送信", function(){
+    context("成功した場合", function(){
+
+      it("callback でレスポンスを取得できる", function(done){
+        ncmb.User.requestPasswordReset("test@example.com", function(err, data){
+          done(err ? err : null);
+        });
+      });
+
+      it("promise でレスポンスを取得できる", function(done){
+        ncmb.User.requestPasswordReset("test@example.com")
+        .then(function(data){
+          done();
+        })
+        .catch(function(err){
+          done(err);
+        });
+      });
+    });
+
+    context("失敗した理由が", function(){
+      context("mailAddress がないときに", function(){
+
+        it("callback で送信時エラーを取得できる", function(done){
+          ncmb.User.requestPasswordReset(null, function(err, data){
+            try{
+              expect(err).to.be.an.instanceof(Error);
+            }catch(err){
+              done(err);
+            }
+            done();
+          });
+        });
+
+        it("promise で送信時エラーを取得できる", function(done){
+          ncmb.User.requestPasswordReset()
+          .then(function(data){
+             done(new Error("失敗すべき"));
+          })
+          .catch(function(err){
+            try{
+              expect(err).to.be.an.instanceof(Error);
+            }catch(err){
+              done(err);
+            }
+            done();
+          });
+        });
+      });
+    });
+  });
+
   describe("ユーザー削除", function(){
     context("成功した場合", function(){
       var del_user = new ncmb.User({objectId: "object_id"});

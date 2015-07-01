cb82fdbafdc9dcbfe1b36bf0dff250d2dffb20a5
4989 <mbaascommit@gmail.com>
2015-07-01 11:56:27 +0900

    [FIX] fixed structure of acl test

--- test/acl_test.js
+++ test/acl_test.js
@@ -25,39 +25,39 @@ describe("NCMB ACL", function(){
       });
     });
 
-    describe("権限を設定チェック", function() {
-      it("Public Readを指定し、取得できる", function() {
-        var aclObj1 = new ncmb.Acl();
-        aclObj1.setPublicReadAccess(true);
-        expect(aclObj1.toJSON()).to.be.eql({"*":{read: true}});
-      });
-      it("Public Writeを指定し、取得できる", function() {
-        var aclObj2 = new ncmb.Acl();
-        aclObj2.setPublicWriteAccess(true);
-        expect(aclObj2.toJSON()).to.be.eql({"*":{write: true}});
-      });
-      it("Public Write, Public Readを指定し、取得できる", function() {
-        var aclObj = new ncmb.Acl();
-        aclObj.setPublicReadAccess(true);
-        aclObj.setPublicWriteAccess(true);
-        expect(aclObj.toJSON()).to.be.eql({"*":{read: true, write: true}});
-      });
-      it("Public Write true, Public Write falseを連続指定し、最後に指定したfalseを取得できる", function() {
-        var aclObj = new ncmb.Acl();
-        aclObj.setPublicWriteAccess(true);
-        aclObj.setPublicWriteAccess(false);
-        expect(aclObj.toJSON()).to.be.eql({"*":{write: false}});
-      });
-      it("Public Read true, Public Read falseを連続指定し、最後に指定したfalseを取得できる", function() {
-        var aclObj = new ncmb.Acl();
-        aclObj.setPublicReadAccess(true);
-        aclObj.setPublicReadAccess(false);
-        expect(aclObj.toJSON()).to.be.eql({"*":{read: false}});
-      });
-      it("Public Read true, Public Read falseをchain指定し、最後に指定したfalseを取得できる", function() {
-        var aclObj = new ncmb.Acl();
-        aclObj.setPublicReadAccess(true).setPublicWriteAccess(false);
-        expect(aclObj.toJSON()).to.be.eql({"*":{read: true, write: false}});
+    describe("権限の設定チェック", function() {
+      var aclObj = null;
+      context("Public権限に対して", function(){
+        beforeEach(function(){
+          aclObj = new ncmb.Acl();
+        });
+        it("Readを指定し、取得できる", function() {
+          aclObj.setPublicReadAccess(true);
+          expect(aclObj.toJSON()).to.be.eql({"*":{read: true}});
+        });
+        it("Writeを指定し、取得できる", function() {
+          aclObj.setPublicWriteAccess(true);
+          expect(aclObj.toJSON()).to.be.eql({"*":{write: true}});
+        });
+        it("Write, Readを指定し、取得できる", function() {
+          aclObj.setPublicReadAccess(true);
+          aclObj.setPublicWriteAccess(true);
+          expect(aclObj.toJSON()).to.be.eql({"*":{read: true, write: true}});
+        });
+        it("Write true, Write falseを連続指定し、最後に指定したfalseを取得できる", function() {
+          aclObj.setPublicWriteAccess(true);
+          aclObj.setPublicWriteAccess(false);
+          expect(aclObj.toJSON()).to.be.eql({"*":{write: false}});
+        });
+        it("Read true, Read falseを連続指定し、最後に指定したfalseを取得できる", function() {
+          aclObj.setPublicReadAccess(true);
+          aclObj.setPublicReadAccess(false);
+          expect(aclObj.toJSON()).to.be.eql({"*":{read: false}});
+        });
+        it("Read true, Read falseをchain指定し、最後に指定したfalseを取得できる", function() {
+          aclObj.setPublicReadAccess(true).setPublicWriteAccess(false);
+          expect(aclObj.toJSON()).to.be.eql({"*":{read: true, write: false}});
+        });
       });
     });
 

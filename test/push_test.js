0f0e853c957f5fa414480aae9bf3272e85165cb2
4989 <mbaascommit@gmail.com>
2015-08-13 10:49:36 +0900

    [FIX] fixed push send and test

--- test/push_test.js
+++ test/push_test.js
@@ -6,21 +6,58 @@ var expect   = require("chai").expect;
 var NCMB = require("../lib/ncmb");
 
 describe("NCMB Push", function(){
-  it("create push", function(done){
-    var ncmb = new NCMB(config.apikey, config.clientkey );
+  var ncmb = null;
+  before(function(){
+    ncmb = new NCMB(config.apikey, config.clientkey );
     if(config.apiserver){
-      ncmb
-      .set("protocol", config.apiserver.protocol || "http:")
-      .set("fqdn", config.apiserver.fqdn)
-      .set("port", config.apiserver.port)
-      .set("proxy", config.apiserver.port || "");
+      ncmb.set("protocol", config.apiserver.protocol || "http:")
+          .set("fqdn", config.apiserver.fqdn)
+          .set("port", config.apiserver.port)
+          .set("proxy", config.apiserver.port || "");
     }
-
-    var p = new ncmb.Push({immediateDeliveryFlag: true});
-    p.send().then(function(push){
+  });
+  var push = null;
+  describe("インスタンス生成", function(){
+    it("プロパティをconstructorで指定できる", function(done){
+      push = new ncmb.Push({immediateDeliveryFlag: true});
+      expect(push.immediateDeliveryFlag).to.be.eql(true);
       done();
-    }).catch(function(err){
-      done(err);
+    });
+    it("変更できないプロパティを指定するとエラーが返る", function(done){
+      try{
+        push = new ncmb.Push({send: true});
+        done(new Error("失敗すべき"));
+      }catch(err){
+        done();
+      };
+    });
+  });
+  describe("プッシュ送信", function(){
+    context("プッシュ通知を送信したとき、送信に成功して", function(){
+      beforeEach(function(){
+        push = new ncmb.Push({immediateDeliveryFlag: true});
+      });
+      it("callback で取得できる", function(done){
+        push.send(function(err, obj){
+          if(err){
+            done(err);
+          }else{
+            expect(obj.objectId).to.be.eql("push_id");
+            done();
+          }
+        });
+      });
+      it("promise で取得できる", function(done){
+        
+        push.send()
+            .then(function(obj){
+              expect(obj.objectId).to.be.eql("push_id");
+              done();
+            })
+            .catch(function(err){
+              done(err);
+            });
+      });
     });
   });
 });

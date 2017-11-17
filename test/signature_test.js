4364643cf9d6bd3c73bbab381055fdbc9ecf397a
ThanhNH <thanhnh@vitalify.asia>
2017-11-17 11:59:17 +0900

    Add check response signature data in stub

--- test/signature_test.js
+++ test/signature_test.js
@@ -4,9 +4,24 @@ var config   = require("config");
 var expect   = require("chai").expect;
 
 var signature = require("../lib/signature");
+var NCMB = require("../lib/ncmb");
+var ncmb = null;
 
 (typeof window !== "undefined" ? describe.skip : describe)("NCMB signature", function(){
   describe("create", function(){
+    var name_user = null;
+    before(function(){
+      ncmb = new NCMB(config.apikey, config.clientkey );
+      if(config.apiserver){
+        ncmb
+        .set("protocol", config.apiserver.protocol)
+        .set("fqdn", config.apiserver.fqdn)
+        .set("port", config.apiserver.port)
+        .set("proxy", config.apiserver.proxy || "")
+        .set("stub", config.apiserver.stub);
+      }
+      name_user = new ncmb.User({userName: "Yamada Tarou", password:"password"});
+    });
     it("[公式ドキュメント](http://bit.ly/1GsvAKL) の通りに成功する", function(){
       var sig = signature.create(
         "https://mb.api.cloud.nifty.com/2013-09-01/classes/TestClass",
@@ -19,6 +34,37 @@ var signature = require("../lib/signature");
         "1343d198b510a0315db1c03f3aa0e32418b7a743f8e4b47cbff670601345cf75"
       );
       expect(sig).to.be.equal("/mQAJJfMHx2XN9mPZ9bDWR9VIeftZ97ntzDIRw0MQ4M=");
+    });
+    it("レスポンスシグネチャチェック", function(done){
+      ncmb.enableResponseValidation(true);
+      ncmb.createSignature(
+        "https://"+ncmb.fqdn+"/" + ncmb.version +"/users",
+        "POST",
+        {userName: "Yamada Tarou", password:"password"},
+        "2017-11-16T09:56:28.373Z"
+      );
+    
+      ncmb.request({
+        path: "/" + ncmb.version + "/users",
+        method: "POST",
+        data: name_user,
+        timestamp: "2017-11-16T09:56:28.373Z"
+      }).then(function(data){
+        ncmb.enableResponseValidation(false);
+        var obj = null;
+        try{
+          obj = JSON.parse(data);
+        }catch(err){
+          throw err;
+        }
+        if(obj == null) return done("Null response data");
+        done();
+        return name_user;
+      }.bind(name_user)).catch(function(err){
+        if(err) return done(err);
+        throw err;
+      });
+
     });
   });
 });

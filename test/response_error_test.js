b19869f28ae67f559292fc507d0d0d776d0f8eec
vfa.duyna <vfa.duyna@gmail.com>
2017-12-19 17:20:22 +0900

    Update source code and rename files.

--- test/response_error_test.js
+++ test/response_error_test.js
@@ -1,99 +1,99 @@
 "use strict";
 
-var config   = require("config");
-var expect   = require("chai").expect;
+var config = require("config");
+var expect = require("chai").expect;
 
 var NCMB = require("../lib/ncmb");
 
-describe("NCMB Response error", function(){
-  var ncmb = null;
-  before(function(){
-    ncmb = new NCMB(config.apikey, config.clientkey);
-    if(config.apiserver){
-      ncmb
-      .set("protocol", config.apiserver.protocol)
-      .set("fqdn", config.apiserver.fqdn)
-      .set("port", config.apiserver.port)
-      .set("proxy", config.apiserver.proxy || "")
-      .set("stub", config.apiserver.stub);
-    }
-  });
+describe("レスポンスエラー確認", function() {
 
-
-  describe("Error login API", function(){
-    var user = null;
-    var userName = null;
-    var password = null;
-
-    context("test response error login ", function(){
-      beforeEach(function(){
-        userName = "wronguser"
-        password = "wrongpassword";
-        user = new ncmb.User({userName: userName,password: password});
-      });
-     
-      it("callback error login", function(done){
-        ncmb.User.login(user, function(err, data){
-          if(!err){
-            done();
-          }else{
-            expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
-            done();
-          }
+    describe("ID/PWユーザーでログイン", function() {
+        var ncmb = null;
+        before(function() {
+            ncmb = new NCMB(config.apikey, config.clientkey);
+            if (config.apiserver) {
+                ncmb
+                    .set("protocol", config.apiserver.protocol)
+                    .set("fqdn", config.apiserver.fqdn)
+                    .set("port", config.apiserver.port)
+                    .set("proxy", config.apiserver.proxy || "")
+                    .set("stub", config.apiserver.stub);
+            }
         });
-      });
 
-      it("promise error login", function(done){
-        ncmb.User.login(user)
-        .then(function(data){
-          done();
-        })
-        .catch(function(err){
-          expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
-          done();
-        });
-      });
-    });
-  });
-
-
-  describe("No such application API", function(){
-    var user = null;
-    var userName = null;
-    var password = null;
-    before(function(){
-      ncmb = new NCMB("wrongAPIKEY", "wrongCLIENTKEY" );
-    });
-
-    context("test response error No such application", function(){
-      var TestClassError = null;
-      var testClassError = null;
-      beforeEach(function(){
-        TestClassError = ncmb.DataStore("TestClassError");
-        testClassError = new TestClassError({key: "value"});
-      });
-      it("callback test response error", function(done){
-        testClassError.save(function(err, obj){
-          if(err){
-            expect(err.error).to.equal('No such application.');
-            done();
-          }else{
-            done();
-          }
-        });
-      });
-      it("promise test response error", function(done){
-        testClassError.save()
-            .then(function(obj){
-              done();
-            })
-            .catch(function(err){
-              expect(err.error).to.equal('No such application.');
-              done();
+        var user = null;
+        var userName = null;
+        var password = null;
+        context("プロバティにuserName, passwordが間違ってログインに失敗して", function() {
+            beforeEach(function() {
+                userName = "wronguser"
+                password = "wrongpassword";
+                user = new ncmb.User({
+                    userName: userName,
+                    password: password
+                });
             });
-      });
+
+            it("callback でレスポンスのエラーを確認できる", function(done) {
+                ncmb.User.login(user, function(err, data) {
+                    if (!err) {
+                        done();
+                    } else {
+                        expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
+                        done();
+                    }
+                });
+            });
+
+            it("promise でレスポンスのエラーを確認できる", function(done) {
+                ncmb.User.login(user)
+                    .then(function(data) {
+                        done();
+                    })
+                    .catch(function(err) {
+                        expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
+                        done();
+                    });
+            });
+        });
     });
 
-  });
 
+    describe("パラメータ設定", function() {
+        var ncmb = null;
+        before(function() {
+            ncmb = new NCMB("wrongAPIKEY", "wrongCLIENTKEY");
+        });
+
+        context("設定可能でないキーの場合は失敗する", function() {
+            var TestClassError = null;
+            var testClassError = null;
+            beforeEach(function() {
+                TestClassError = ncmb.DataStore("WrongAppKeyAndClientKey");
+                testClassError = new TestClassError({
+                    key: "value"
+                });
+            });
+            it("callback でレスポンスのエラーを確認できる", function(done) {
+                testClassError.save(function(err, obj) {
+                    if (err) {
+                        expect(err.error).to.equal('No such application.');
+                        done();
+                    } else {
+                        done();
+                    }
+                });
+            });
+            it("promise でレスポンスのエラーを確認できる", function(done) {
+                testClassError.save()
+                    .then(function(obj) {
+                        done();
+                    })
+                    .catch(function(err) {
+                        expect(err.error).to.equal('No such application.');
+                        done();
+                    });
+            });
+        });
+    });
 });

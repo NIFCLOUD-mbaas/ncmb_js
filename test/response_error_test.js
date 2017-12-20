a4701c2ad8d4a860e61a4bdbbcf6ba3ad8b7ecf3
vfa.duyna <vfa.duyna@gmail.com>
2017-12-20 16:54:40 +0700

    request.js: Revert deletion response_error_test.js: Make failure for no-reached code.

--- test/response_error_test.js
+++ test/response_error_test.js
@@ -37,7 +37,7 @@ describe("レスポンスエラー確認", function() {
             it("callback でレスポンスのエラーを確認できる", function(done) {
                 ncmb.User.login(user, function(err, data) {
                     if (!err) {
-                        done();
+                        done(new Error("失敗すべき"));
                     } else {
                         expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
                         done();
@@ -48,7 +48,7 @@ describe("レスポンスエラー確認", function() {
             it("promise でレスポンスのエラーを確認できる", function(done) {
                 ncmb.User.login(user)
                     .then(function(data) {
-                        done();
+                        done(new Error("失敗すべき"));
                     })
                     .catch(function(err) {
                         expect(err.error).to.equal('Authentication error with ID/PASS incorrect.');
@@ -80,14 +80,14 @@ describe("レスポンスエラー確認", function() {
                         expect(err.error).to.equal('No such application.');
                         done();
                     } else {
-                        done();
+                        done(new Error("失敗すべき"));
                     }
                 });
             });
             it("promise でレスポンスのエラーを確認できる", function(done) {
                 testClassError.save()
                     .then(function(obj) {
-                        done();
+                        done(new Error("失敗すべき"));
                     })
                     .catch(function(err) {
                         expect(err.error).to.equal('No such application.');
